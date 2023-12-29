fb0e6171622cb37df0bb5340474d631feacc8b8c
LonebirdRamin <dsl0nebird@gmail.com>
2023-12-30 00:34:14 +0700

    Finish introduction

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 // Profile image change over time
 document.addEventListener('DOMContentLoaded', function () {
-    // //Array of image sources
-    // var imgSources = ["ramin_cpe_openhouse.jpg", "ramin_cpe_openhouse_2.jpg"];
-
-    // //Get the image element
-    // var img = document.getElementById("self_image");
-
-    // //Set an index to keep track of the current image
-    // var currentImg = 0;
-
-    // //Function to change the image
-    // function changeImg() {
-    //     img.style.clipPath = 'polygon(0 0, 0 100%, 100% 100%, 100% 0)';
-    //     setTimeout(function () {
-    //         img.src = "./resource/image/" + imgSources[currentImg];
-    //         img.style.clipPath = 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'; // Reset the left property after changing the image
-    //         currentImg = (currentImg + 1) % imgSources.length;
-    //     }, 500); // Adjust the timeout to match the transition duration
-    // }
-
-    // //Set an interval to change the image every 10 seconds
-    // var intervaldId = setInterval(changeImg, 2000);
     var carouselImages = document.getElementById("self_images");
     var imageWidth = document.querySelector(".self_image").clientWidth;
     var currentImageIndex = 0;
@@ -39,5 +18,44 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     // Change image every 3 seconds
     var intervalId = setInterval(changeImg, 3000);
-});
 
+    // Change URL based on scroll position
+    window.addEventListener('scroll', function () {
+        var scrollPosition = window.scrollY;
+        var sections = [
+            { id: 'home', offset: 0 },
+            { id: 'introduction', offset: document.getElementById('home').offsetHeight },
+            { id: 'skill', offset: document.getElementById('home').offsetHeight + document.getElementById('introduction').offsetHeight },
+            { id: 'experience', offset: document.getElementById('home').offsetHeight + document.getElementById('introduction').offsetHeight + document.getElementById('skill').offsetHeight },
+            { id: 'education', offset: document.getElementById('home').offsetHeight + document.getElementById('introduction').offsetHeight + document.getElementById('skill').offsetHeight + document.getElementById('experience').offsetHeight },
+            { id: 'contact', offset: document.getElementById('home').offsetHeight + document.getElementById('introduction').offsetHeight + document.getElementById('skill').offsetHeight + document.getElementById('experience').offsetHeight + document.getElementById('education').offsetHeight }
+        ]
+
+        // Find the current section based on the scroll position
+        var currentSection = sections.find(function (section, index) {
+            var nextSection = sections[index + 1];
+            return scrollPosition >= section.offset && (!nextSection || scrollPosition < nextSection.offset);
+        });
+
+        // If no current section is found, assume it's in the 'home' section
+        if (!currentSection) {
+            currentSection = sections[0];
+        }
+
+        // Update the URL with the current section's ID
+        history.replaceState({}, '', '#' + currentSection.id);
+
+        // Remove the 'navbox_active' class from all navboxes
+        document.querySelectorAll('.navbox').forEach(function (navbox) {
+            navbox.classList.remove('navbox_active');
+        });
+
+        // Add the 'navbox_active' class to the navbox corresponding to the current section
+        var currentNavbox = document.querySelector('.navbox a[href="#' + currentSection.id + '"]').parentNode;
+        if (currentNavbox) {
+            currentNavbox.classList.add('navbox_active');
+        }
+    });
+
+    window.scrollTo(0, 0);
+});
